d4bf78cda97041ccb98eca1371ac5baab2abf91f
RevoltSecurities <119435129+RevoltSecurities@users.noreply.github.com>
2025-05-13 16:28:56 +0530

    Delete shodanx directory

--- shodanx/modules/verify/verify.py
+++
@@ -1,8 +0,0 @@
-import importlib.metadata as data
-
-
-def verify(pkg): #After version verified call the updatelog for updates information
-    
-    version = data.version(pkg)
-    
-    return version
